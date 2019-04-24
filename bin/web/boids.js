d289e2b3264b0a5ae98b9fdb7789a6b232dee8af
mtoutside <more.humor.or@gmail.com>
2019-04-24 20:22:18 +0900

    ・ゲームオーバー時の効果音追加 ・不要なコードとコメントの整理

--- bin/web/boids.js
+++ bin/web/boids.js
@@ -4,7 +4,6 @@ let sketch = function(s) {
     let player;
     let enemy = [];
     let count = 0;
-    // let gameOver = false;
     const wrapper = document.querySelector('.menu__wrapper');
     const title = document.querySelector('.titlearea');
     const gl = snow_modules_opengl_web_GL.gl;
@@ -30,8 +29,12 @@ let sketch = function(s) {
         };
         swapVertShader(gpu_fluid_main.renderParticlesShader, "/shaders/glsl/renderparticleshader.vert");
 
+        // サウンドファイル
         bgm = s.loadSound('/assets/sounds/bgm.mp3');
         eat = s.loadSound('/assets/sounds/eat.mp3');
+        end = s.loadSound('/assets/sounds/end.mp3');
+
+        //フォントファイル
         myFont = s.loadFont('/assets/fonts/PressStart2P.ttf');
     }
 
@@ -48,7 +51,7 @@ let sketch = function(s) {
         player = new Player();
         enemy.push(new Enemy());
 
-        bgm.setVolume(0.3);
+        bgm.setVolume(0.3); //音量調整
         bgm.loop();
     };
 
@@ -85,12 +88,16 @@ let sketch = function(s) {
 
         for(let i in enemy) {
             enemy[i].render();
-            // 敵との当たり判定
+
+            // 敵とプレイヤーの当たり判定
             if(enemy[i].hits(player)) {
                 enemy = [];
                 flock.boids = [];
                 gameOver = true;
                 isRunning = false;
+                end.play();
+
+                // 結果画面生成
                 endText = s.createP('GAME OVER');
                 scoreText = s.createP(`Score: ${count}`);
                 endText.class('title');
@@ -102,6 +109,7 @@ let sketch = function(s) {
                 break;
 
             }
+
             enemy[i].arrive(player.position.x, player.position.y);
             enemy[i].separate(enemy);
             enemy[i].update();
@@ -114,7 +122,8 @@ let sketch = function(s) {
         player.update();
         player.edges();
 
-        s.textSize(32);
+        // 左上のスコア表示
+        s.textSize(30);
         s.textFont(myFont);
         s.fill(242, 58, 12);
         s.text(`Score:${count}`, 10, 50);
@@ -152,7 +161,6 @@ let sketch = function(s) {
             }
         }
 
-
 		this.boosting = function(b) {
 			this.isBoosting = b;
 		}
@@ -163,7 +171,6 @@ let sketch = function(s) {
 			}
             
             this.theta += s.PI / 100;
-
 			this.position.add(this.vel);
 			this.vel.mult(0.95);
 		}
@@ -228,7 +235,6 @@ let sketch = function(s) {
             s.bezierVertex(0, 0, 10, 10, 0, 30);
             s.endShape();
             s.pop();
-
         }
 
         this.edges = function() {
@@ -237,6 +243,7 @@ let sketch = function(s) {
             if (this.position.x > s.width + this.r) this.position.x = -this.r;
             if (this.position.y > s.height + this.r) this.position.y = -this.r;
         }
+
         this.setRotation = function(a) {
             this.rotation = a;
         }
@@ -259,37 +266,28 @@ let sketch = function(s) {
         this.heading = s.radians(90);
         this.color = { filet: s.color(133, 255, 14), body: s.color(144, 169, 122) };
         this.acceleration = s.createVector(0, 0);
-        this.maxspeed = s.random(3, 5);    // Maximum speed
-        this.maxforce = 0.3; // Maximum steering force
+        this.maxspeed = s.random(4, 8);
+        this.maxforce = 0.4;
 
         this.applyForce = function(force) {
             this.acceleration.add(force);
         };
 
-        // Method to update location
         this.update = function() {
-            // Update velocity
             this.velocity.add(this.acceleration);
-            // Limit speed
             this.velocity.limit(this.maxspeed);
             this.position.add(this.velocity);
-            // Reset accelertion to 0 each cycle
             this.acceleration.mult(0);
-
             this.theta += s.PI / 100;
         };
 
-        // A method that calculates and applies a steering force towards a target
-        // STEER = DESIRED MINUS VELOCITY
         this.seek = function(target) {
-            let desired = p5.Vector.sub(target,this.position);  // A vector pointing from the location to the target
+            let desired = p5.Vector.sub(target,this.position);
             this.heading = s.atan2(desired.y, desired.x) + s.radians(90);
-            // Normalize desired and scale to maximum speed
             desired.normalize();
             desired.mult(this.maxspeed);
-            // Steering = Desired minus Velocity
             let steer = p5.Vector.sub(desired,this.velocity);
-            steer.limit(this.maxforce);  // Limit to maximum steering force
+            steer.limit(this.maxforce);
             return steer;
         };
 
@@ -302,7 +300,7 @@ let sketch = function(s) {
             let steer = s.createVector(0, 0);
 
             if ((d > 0) && (d < neighbordist)) {
-                steer = this.seek(target);  // Chaseing mouse
+                steer = this.seek(target);
             }
             steer.mult(3);
             steer.limit(this.maxforce);
@@ -328,19 +326,15 @@ let sketch = function(s) {
                 if ((d > 0) && (d < desiredseparation)) {
                     let diff = p5.Vector.sub(this.position, enemy[i].position);
                     diff.normalize();
-                    diff.div(d);        // Weight by distance
+                    diff.div(d);
                     steer.add(diff);
-                    count++;            // Keep track of how many
+                    count++;
                 }
             }
-            // Average -- divide by how many
             if (count > 0) {
                 steer.div(count);
             }
-
-            // As long as the vector is greater than 0
             if (steer.mag() > 0) {
-                // Implement Reynolds: Steering = Desired - Velocity
                 steer.normalize();
                 steer.mult(this.maxspeed);
                 steer.sub(this.velocity);
@@ -437,24 +431,15 @@ let sketch = function(s) {
         this.boids.push(b);
     };
 
-    // The Nature of Code
-    // Daniel Shiffman
-    // http://natureofcode.com
-
-    // Boid class
-    // Methods for Separation, Cohesion, Alignment added
-
-    // Reading GPU Colors
+    // 速度場のピクセルを読み取る
     readVelocityAt = function (x, y) {
         let pixel = new Float32Array(4); //1pxの情報を格納する配列 RGBAだから4.
         let velocityFBO = gpu_fluid_main.fluid.velocityRenderTarget.readFrameBufferObject;
-
         gl.bindFramebuffer(gl.FRAMEBUFFER, velocityFBO);   //velocityFBOをcurrentに
         gl.readPixels(Math.floor(fluidFieldScale.w * x),
             Math.floor(fluidFieldScale.h * y),
             1,1,gl.RGBA, gl.FLOAT, pixel); //pixelに読み込む
         gl.bindFramebuffer(gl.FRAMEBUFFER, null);          //FBOを戻す
-
         return s.createVector( pixel[0], pixel[1] );
     };
 
@@ -492,7 +477,7 @@ let sketch = function(s) {
         // Arbitrarily weight these forces
         sep.mult(1.5);
         ali.mult(1.0);
-        coh.mult(1.0);
+        coh.mult(0.8);
         // Add the force vectors to acceleration
         this.applyForce(sep);
         this.applyForce(ali);
@@ -500,7 +485,7 @@ let sketch = function(s) {
 
     };
 
-    //follow the fluid
+    // 速度場の流れに従う
     Boid.prototype.follow = function() {
         let desired = readVelocityAt(Math.floor(window.innerWidth - this.position.x), Math.floor(window.innerHeight - this.position.y));
         desired.normalize();
@@ -510,7 +495,6 @@ let sketch = function(s) {
         steer.mult(1.5);
         steer.limit(this.maxforce);
         this.applyForce(steer);
-        // this.position.add(steer);
     }
 
     // Method to update location
@@ -527,125 +511,124 @@ let sketch = function(s) {
     // A method that calculates and applies a steering force towards a target
     // STEER = DESIRED MINUS VELOCITY
     Boid.prototype.seek = function(target) {
-	let desired = p5.Vector.sub(target,this.position);  // A vector pointing from the location to the target
-	// Normalize desired and scale to maximum speed
-	desired.normalize();
-	desired.mult(this.maxspeed);
-	// Steering = Desired minus Velocity
-	let steer = p5.Vector.sub(desired,this.velocity);
-	steer.limit(this.maxforce);  // Limit to maximum steering force
-	return steer;
+        let desired = p5.Vector.sub(target,this.position);  // A vector pointing from the location to the target
+        // Normalize desired and scale to maximum speed
+        desired.normalize();
+        desired.mult(this.maxspeed);
+        // Steering = Desired minus Velocity
+        let steer = p5.Vector.sub(desired,this.velocity);
+        steer.limit(this.maxforce);  // Limit to maximum steering force
+        return steer;
     };
 
     Boid.prototype.render = function() {
-	// Draw a triangle rotated in the direction of velocity
-	let theta = this.velocity.heading() + s.radians(90);
-    s.noStroke();
-	s.fill(170, 120, 155);
-	s.push();
-	s.translate(this.position.x, this.position.y);
-	s.rotate(theta);
-	s.beginShape();
-	// s.vertex(this.r, this.r * 2);
-    s.curveVertex(0, -1.5 * this.r * 1.0);
-    s.curveVertex(0.5 * this.r * 1.5, 0);
-    s.curveVertex(0, 2.5 * this.r * 1.5);
-    s.curveVertex(-0.5 * this.r * 1.5, 0);
-    s.curveVertex(0, -1.5 * this.r * 1.0);
-    s.curveVertex(0.5 * this.r * 1.5, 0);
-    s.curveVertex(0, 2.5 * this.r * 1.5);
-	s.endShape(s.CLOSE);
-	s.pop();
+        // Draw a triangle rotated in the direction of velocity
+        let theta = this.velocity.heading() + s.radians(90);
+        s.noStroke();
+        s.fill(170, 120, 155);
+        s.push();
+        s.translate(this.position.x, this.position.y);
+        s.rotate(theta);
+        s.beginShape();
+        s.curveVertex(0, -1.5 * this.r * 1.0);
+        s.curveVertex(0.5 * this.r * 1.5, 0);
+        s.curveVertex(0, 2.5 * this.r * 1.5);
+        s.curveVertex(-0.5 * this.r * 1.5, 0);
+        s.curveVertex(0, -1.5 * this.r * 1.0);
+        s.curveVertex(0.5 * this.r * 1.5, 0);
+        s.curveVertex(0, 2.5 * this.r * 1.5);
+        s.endShape(s.CLOSE);
+        s.pop();
     };
 
     // Wraparound
     Boid.prototype.borders = function() {
-	if (this.position.x < -this.r)  this.position.x = s.width + this.r;
-	if (this.position.y < -this.r)  this.position.y = s.height + this.r;
-	if (this.position.x > s.width + this.r) this.position.x = -this.r;
-	if (this.position.y > s.height + this.r) this.position.y = -this.r;
+        if (this.position.x < -this.r)  this.position.x = s.width + this.r;
+        if (this.position.y < -this.r)  this.position.y = s.height + this.r;
+        if (this.position.x > s.width + this.r) this.position.x = -this.r;
+        if (this.position.y > s.height + this.r) this.position.y = -this.r;
     };
 
     // Separation
     // Method checks for nearby boids and steers away
     Boid.prototype.separate = function(boids) {
-	let desiredseparation = 25.0;
-	let steer = s.createVector(0, 0);
-	let count = 0;
-	// For every boid in the system, check if it's too close
-	for (let i = 0; i < boids.length; i++) {
-	    let d = p5.Vector.dist(this.position,boids[i].position);
-	    // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
-	    if ((d > 0) && (d < desiredseparation)) {
-		// Calculate vector pointing away from neighbor
-		let diff = p5.Vector.sub(this.position, boids[i].position);
-		diff.normalize();
-		diff.div(d);        // Weight by distance
-		steer.add(diff);
-		count++;            // Keep track of how many
-	    }
-	}
-	// Average -- divide by how many
-	if (count > 0) {
-	    steer.div(count);
-	}
+        let desiredseparation = 25.0;
+        let steer = s.createVector(0, 0);
+        let count = 0;
+        // For every boid in the system, check if it's too close
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.position,boids[i].position);
+            // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+            if ((d > 0) && (d < desiredseparation)) {
+                // Calculate vector pointing away from neighbor
+                let diff = p5.Vector.sub(this.position, boids[i].position);
+                diff.normalize();
+                diff.div(d);        // Weight by distance
+                steer.add(diff);
+                count++;            // Keep track of how many
+            }
+        }
+        // Average -- divide by how many
+        if (count > 0) {
+            steer.div(count);
+        }
 
-	// As long as the vector is greater than 0
-	if (steer.mag() > 0) {
-	    // Implement Reynolds: Steering = Desired - Velocity
-	    steer.normalize();
-	    steer.mult(this.maxspeed);
-	    steer.sub(this.velocity);
-	    steer.limit(this.maxforce);
-	}
-	return steer;
+        // As long as the vector is greater than 0
+        if (steer.mag() > 0) {
+            // Implement Reynolds: Steering = Desired - Velocity
+            steer.normalize();
+            steer.mult(this.maxspeed);
+            steer.sub(this.velocity);
+            steer.limit(this.maxforce);
+        }
+        return steer;
     };
 
     // Alignment
     // For every nearby boid in the system, calculate the average velocity
     Boid.prototype.align = function(boids) {
-	let neighbordist = 50;
-	let sum = s.createVector(0,0);
-	let count = 0;
-	for (let i = 0; i < boids.length; i++) {
-	    let d = p5.Vector.dist(this.position,boids[i].position);
-	    if ((d > 0) && (d < neighbordist)) {
-		sum.add(boids[i].velocity);
-		count++;
-	    }
-	}
-	if (count > 0) {
-	    sum.div(count);
-	    sum.normalize();
-	    sum.mult(this.maxspeed);
-	    let steer = p5.Vector.sub(sum, this.velocity);
-	    steer.limit(this.maxforce);
-	    return steer;
-	} else {
-	    return s.createVector(0, 0);
-	}
+        let neighbordist = 50;
+        let sum = s.createVector(0,0);
+        let count = 0;
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.position,boids[i].position);
+            if ((d > 0) && (d < neighbordist)) {
+                sum.add(boids[i].velocity);
+                count++;
+            }
+        }
+        if (count > 0) {
+            sum.div(count);
+            sum.normalize();
+            sum.mult(this.maxspeed);
+            let steer = p5.Vector.sub(sum, this.velocity);
+            steer.limit(this.maxforce);
+            return steer;
+        } else {
+            return s.createVector(0, 0);
+        }
     };
 
     // Cohesion
     // For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
     Boid.prototype.cohesion = function(boids) {
-	let neighbordist = 50;
-	let sum = s.createVector(0, 0);   // Start with empty vector to accumulate all locations
-	let count = 0;
-	for (let i = 0; i < boids.length; i++) {
-	    let d = p5.Vector.dist(this.position,boids[i].position);
-	    if ((d > 0) && (d < neighbordist)) {
-	    // sum.add(s.mouseX, s.mouseY);  // Chaseing mouse
-	     sum.add(boids[i].position); // Add location
-	    count++;
-	    }
-	}
-	if (count > 0) {
-	    sum.div(count);
-	    return this.seek(sum);  // Steer towards the location
-	} else {
-	    return s.createVector(0, 0);
-	}
+        let neighbordist = 50;
+        let sum = s.createVector(0, 0);   // Start with empty vector to accumulate all locations
+        let count = 0;
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.position,boids[i].position);
+            if ((d > 0) && (d < neighbordist)) {
+                // sum.add(s.mouseX, s.mouseY);  // Chaseing mouse
+                sum.add(boids[i].position); // Add location
+                count++;
+            }
+        }
+        if (count > 0) {
+            sum.div(count);
+            return this.seek(sum);  // Steer towards the location
+        } else {
+            return s.createVector(0, 0);
+        }
     };
 
     // Avoid
@@ -658,7 +641,7 @@ let sketch = function(s) {
         let steer = s.createVector(0, 0);
 
         if ((d > 0) && (d < neighbordist)) {
-            steer = this.seek(target);  // Chaseing mouse
+            steer = this.seek(target);
             steer.mult(-1);
         }
         steer.mult(3);
